5b1cd720c941196086dfdc0c4143ffb41ad78a45
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2022-11-04 19:26:16 +0530

    Create Day1_problem1_Running _Sum _of _1d_Array.cpp

---
+++ Day1_problem1_Running _Sum _of _1d_Array.cpp
@@ -0,0 +1,50 @@
+/*1480. Running Sum of 1d Array
+Easy
+
+5149
+
+265
+
+Add to List
+
+Share
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6*/
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> sum(nums.size(),0);
+        int k;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<=i;j++)
+                sum[k]+=nums[j];
+            k++;
+        }
+        return sum;
+    }
+};
