b186545d6685db0ed611f39516fd6203b359e0ab
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2022-11-06 15:22:27 +0530

    Create Day7_problem1_Binary Search.cpp

---
+++ Day7_problem1_Binary Search.cpp
@@ -0,0 +1,57 @@
+/*704. Binary Search
+Easy
+
+7125
+
+155
+
+Add to List
+
+Share
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order.*/
+
+
+#include<vector>
+class Solution {
+public:
+    int helper(vector<int>& a,int si,int ei,int x){
+        int mid=(si+ei)/2;
+        if(si<=ei){
+        if(x==a[mid])
+            return mid;
+        if(x<a[mid])
+            return helper(a,si,mid-1,x);
+        if(x>a[mid])
+            return helper(a,mid+1,ei,x);
+        }
+        return -1;
+    }
+    int search(vector<int>& a, int x) {
+    int si=0;
+    int ei=a.size()-1;
+    return helper(a,si,ei,x);
+    }
+};
